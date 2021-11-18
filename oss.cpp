cec7b03dd290bcb919776bb868d43436c461d4ca
Christopher Brown <shiningsilence13@gmail.com>
2021-11-18 04:50:51 -0600

    this is depressing

--- oss.cpp
+++ oss.cpp
@@ -3,7 +3,6 @@
 
 	oss.cpp
 
-
 */
 #include <iostream>
 #include <string.h>
@@ -14,7 +13,9 @@
 #include <errno.h>
 #include "sysclock.h"
 #include "oss.h"
+#include "deadlock.h"
 
+#define MAX_PROCESSES 100
 
 static void usage(std::string);
 
@@ -25,11 +26,21 @@ using namespace std;
 
 
 
+volatile sig_atomic_t sig_int_flag = 0;
+
+void signal_handler(int sig)
+{
+	sig_int_flag = 1;
+}
+
+
+
+
 int main(int argc, char* argv[])
 {
 
 	int opt;
-
+	string logfile = "logfile";
 	bool verbose_mode = false;
 
 
@@ -73,11 +84,78 @@ int main(int argc, char* argv[])
 
 static void usage(std::string name)
 {
-	printf("Usage:\t %s [-h].\n", name);
-	printf("Usage:\t %s [-v].\n\n", name);
+	printf("Usage:\t %s [-h].\n", name.c_str());
+	printf("Usage:\t %s [-v].\n\n", name.c_str());
 	printf(" -h  help for invocation and termination\n-v for verbose mode\n\n\n");
 
 
 }
 
 
+int oss(string filename, bool verbose_mode){
+
+	struct SysInfo* sys_info;
+	struct UserProcesses* user_processes;
+	struct ResourceDescriptors* res_desc;
+
+	int wstatus;
+	long next_start_time = 0;
+
+
+	queue<int> ready_queue;
+	list<int> blocked_list;
+
+
+	const pid_t pid = getpid();
+
+
+	srand( time(0) ^ pid );
+
+
+	time_t sec_start = time(NULL);
+
+	int deadlock_timer = 1;
+
+	struct tm * cur_time = localtime( &sec_start );
+
+	if(VerboseMode)
+		print_log("Verbose Mode: ON", logfile);
+	else
+		print_log("Verbose Mode: OFF", logfile);	
+
+
+	bitvector bv(PROCESSES_MAX);
+
+	signal(SIGINT, signal_handler);
+
+	bool killed = false;
+	bool shutdown = false;
+
+	int process_count = 0;
+	int total_time = 0;
+	int count_requested = 0;
+	int count_allocated = 0;
+	int count_released = 0;
+	int count_wait = 0;
+	int count_deadlocked = 0;
+	int count_deadlock_runs = 0;
+	int count_die_nat = 0;
+
+
+	semaphore s(key_mutex, true, 1)
+
+	if(!s.in_init())
+	{
+		perror("ERROR: OSS: unable to create semaphore");
+		exit(EXIT_FAILURE);
+	}
+
+	int msgid = (key_message_queue, IPC_CREAT | 0666);
+	if (msgid == -1)
+	{
+		perror("ERROR: OSS: unable to create message");
+		exit(EXIT_FAILURE);
+	}
+
+
+}
--- sysclock.h
+++ sysclock.h
@@ -26,7 +26,7 @@
 #include <stdarg.h>
 #include <assert.h>
 
-
+using namespace std;
 
 enum RequestType { REQ_CREATE, REQ_DESTROY, REQ_SHUTDOWN, OK};
 
@@ -111,16 +111,82 @@ void print_array( const int* arr, const int arrsize, const int cols )
 	for( int i( 0 ); i < cols; i++ )
 		std::cout << "R" << i << " " << std::endl;
 
-	for( int i( 0 ); i < arrsize/cols: i++)
+	for( int i( 0 ); i < arrsize/cols; i++)
 	{
 		cout << "P" << i << ( ( i > 9 ) ? " " : "  ");
-		for( int j( 0 ); j < cols: j++)
+		for( int j( 0 ); j < cols; j++)
 			cout << arr[i * cols + j] << (( j > 9 ) ? "  " : "  ") << endl;
 	}
 
 }
 
+std::string array_string( const int* arr, const int arrsize, const int cols )
+{
+	std::string output = "   ";
+
+	for( int i( 0 ); i < cols; i++ )
+		output.append("R" + int2str(i) + " \n");
+
+	for( int i( 0 ); i < arrsize/cols; i++)
+	{
+		output.append("P" + int2str(i) + (( i > 9) ? " " : "   "));
+		for( int j( 0 ); j < cols; j++)
+			output.append(int2str(arr[i * cols + j]) + (( j > 9 ) ? "   " : "  "));
+		output.append("\n");
+	}
+	return output;
+}
 
 
+int get_array_value(const int* array, const int row, const int col, const int total_cols)
+{
+	return array[ row * total_cols + col];
 
+}
+
+
+void set_array_value( int* arr, const int row, const int col, const int total_cols, int new_val)
+{
+	arr[ row * total_cols + col] = new_val;
+
+}
+
+std::string print_time(const char* str)
+{
+	time_t t;
+	struct tm * _time;
+	char buffer[10];
+
+	time (&t);
+	_time = localtime ( &t );
+
+
+	strftime( buffer, 80, "%T", _time);
+
+	std::string new_time = str;
+	new_time.append(buffer);
+	
+	return new_time;
+}
+
+bool write_log(std::string& log, std::string filename)
+{
+	std::ofstream log_file (filename.c_str(), std::ofstream::out | std::ofstream::app);
+	if(log_file.is_open())
+	{
+		
+		log_file << " " << log.c_str() << std::endl;
+
+		log_file.close();
+
+		return true;
+	}
+	else
+	{
+		perror("ERROR: failed to write to log");
+		return false;
+	}
+
+
+}
 #endif
